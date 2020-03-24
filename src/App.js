de045a92fbfeebc1c4f91771c3d7f7ebd0d43572
Diego Alzate <diegoff@gmail.com>
2020-03-24 11:16:25 -0500

    Fixing bugs

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
-      </div>
-    );
-  }
+   constructor() {
+      super();
+      this.state = {
+         content: 0
+      };
+
+      this.handleClick = this.handleClick.bind(this);
+   }
+
+   handleClick() {
+      this.setState(prevState => ({
+         content: prevState.content + 1
+      }));
+   }
+
+   render() {
+      return (
+         <div>
+            <span className="value">{this.state.content}</span>
+            <button id="inc" onClick={this.handleClick}>
+               Incrementa
+            </button>
+         </div>
+      );
+   }
 }
 
 export default App;
